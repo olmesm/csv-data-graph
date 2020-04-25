1992be909ba7b73a1d8c827923f32e4b9dd5a219
Oliver Smit <olmesm@gmail.com>
2020-04-25 19:26:56 +0100

    streaming csv to log

--- components/UploadForm.jsx
+++ components/UploadForm.jsx
@@ -3,8 +3,17 @@ import { useState } from "react";
 
 const URL = "/api/upload-csv";
 
+const PROCESS = {
+  INITIAL: "INITIAL",
+  SUCCESS: "SUCCESS",
+  ERROR: "ERROR",
+};
+
+const INITIAL_FILES = [""];
+
 export const SimpleReactFileUpload = () => {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState(INITIAL_FILES);
+  const [process, setProcess] = useState(PROCESS.INITIAL);
 
   const fileUpload = () => {
     const formData = new FormData();
@@ -22,14 +31,49 @@ export const SimpleReactFileUpload = () => {
   const onFormSubmit = (e) => {
     e.preventDefault();
 
-    fileUpload().then((response) => {
-      console.log(response.data);
-    });
+    fileUpload()
+      .then((response) => {
+        console.log(response);
+        setProcess(PROCESS.SUCCESS);
+      })
+      .catch((e) => {
+        console.error(e);
+        setProcess(PROCESS.ERROR);
+      });
   };
 
   const onChange = (e) => {
     setFile(e.target.files[0]);
   };
+
+  const onReset = (e) => {
+    e.preventDefault();
+
+    setProcess(PROCESS.INITIAL);
+    setFile(INITIAL_FILES);
+  };
+
+  if (process === PROCESS.ERROR) {
+    return (
+      <form onSubmit={onReset}>
+        <h1>Error</h1>
+        <p>
+          There appears to be an error. Please try again or contact support.
+        </p>
+        <button>Try again?</button>
+      </form>
+    );
+  }
+
+  if (process === PROCESS.SUCCESS) {
+    return (
+      <form onSubmit={onReset}>
+        <h1>Success</h1>
+        <p>File was uploaded successfully.</p>
+        <button>Upload another?</button>
+      </form>
+    );
+  }
 
   return (
     <form onSubmit={onFormSubmit}>
--- pages/api/upload-csv.js
+++ pages/api/upload-csv.js
@@ -1,4 +1,7 @@
 import formidable from "formidable";
+import { parse as csvParse } from "fast-csv";
+
+const form = formidable();
 
 export const config = {
   api: {
@@ -7,10 +10,26 @@ export const config = {
 };
 
 export default async (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.uploadDir = "./";
-  form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
-    console.log(err, fields, files);
+    // res.send("ok");
+  });
+
+  form.onPart = (part) => {
+    part
+      .pipe(csvParse())
+      .on("error", (error) => {
+        console.error(error);
+      })
+      .on("data", (row) => {
+        console.log(row);
+      });
+    // .on("end", (rowCount) => {
+    //   console.log(`Parsed ${rowCount} rows`);
+    //   res.write("Parsed");
+    // });
+  };
+
+  form.on("end", () => {
+    res.send("OK 1");
   });
 };
